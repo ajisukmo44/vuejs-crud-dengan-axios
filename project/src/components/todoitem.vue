f8148b93449f4ad22b4ffafae390d96fd3529dbe
ajisukmo44 <ajisuk31@gmail.com>
2019-10-17 14:55:16 +0700

    menambah file

---
+++ project/src/components/todoitem.vue
@@ -0,0 +1,14 @@
+
+  <div id="app" style="margin-top:40px">
+  <a-form @submit="addTodo">
+<p>    <label>Nama : </label>    <a-input style="width: 180px; margin-right: 70px" type="text" v-model="todoName" placeholder="Nama"/> </p>
+    <p> <label>Alamat : </label><a-input style="width: 180px; margin-right: 70px" type="text" v-model="todoAlamat"/> </p>
+<button type="submit">SIMPAN</button>
+<br>
+<br>
+  </a-form>
+<hr/>
+<h2 style="margin-top:14px">Data Undangan</h2>
+<p v-for="todo of todos" :key="todo.id">{{todo.id}} - {{todo.name}} - {{todo.alamat}} </p>
+
+</div>
